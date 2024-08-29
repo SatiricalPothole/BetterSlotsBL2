da99e320e5ad076d04a5a3f9d03ad84eaf556a05
SatiricalPothole <lukejonesit@proton.me>
2024-08-29 19:06:18 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,22 @@
+import unrealsdk
+from Mods import ModMenu
+   
+class BetterSlots(ModMenu.SDKMod):
+    Name: str = "BetterSlotsBL2"
+    Author: str = "SatiricalPothole"
+    Description: str = "A mod to make slots easier and more fun to use, roll 10 times simultaneously or use all your cash to roll as many time as you can in once go."
+    Version: str = "1.0.0"
+    SupportedGames: ModMenu.Game = ModMenu.Game.BL2
+    Types: ModMenu.ModTypes = ModMenu.ModTypes.Utility
+    SaveEnabledState: EnabledSaveType = ModMenu.EnabledSaveType.LoadWithSettings
+
+def Enable(self) -> None:
+        super().Enable()
+        unrealsdk.Log("I ARISE!")
+
+def Disable(self) -> None:  # noqa: N802
+        super().Disable()
+        dash.disable()
+        glory_kill.disable()
+   
+ModMenu.RegisterMod(BetterSlots())
